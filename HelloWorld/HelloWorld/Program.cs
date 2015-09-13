7e714f6b38774e11f997ca9de0f064fdfdf10709
BiljanaN91 <nikolovska_biljana91@yahoo.com>
2015-09-13 22:44:41 +0200

    Update Program.cs

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,7 +10,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Yellow World! I made a change");
+            Console.WriteLine("Yellow World! I made a change.And this is two!");
         }
     }
 }
